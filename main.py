1544810af9b41476a6a31116fe7fb80dac871858
11Midnight <47757447+11Midnight@users.noreply.github.com>
2020-09-15 23:57:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import gensim
 import io
 import codecs
 import csv
+import matplotlib.pyplot as plt
 import re
 import nltk
 from gensim.parsing.preprocessing import remove_stopwords
@@ -29,11 +30,26 @@ def analysis(your_list, your_dict):
             your_dict[i] += 1
         else:
             your_dict[i] = 1
-
+def analyslenofword(your_dictone, your_dicttwo):
+    for item in your_dictone:
+        if len(item) in your_dicttwo:
+            your_dicttwo[len(item)] += 1
+        else:
+            your_dicttwo[len(item)] = 1
+    sorted(your_dicttwo)
+    summ = 0
+    sumn = 0
+    for item in your_dicttwo:
+        summ += (int(item) * int(your_dicttwo[item]))
+        sumn += int(your_dicttwo[item])
+    avgvalue = summ / sumn
+    return avgvalue
 spamlist = []
 hamlist = []
 spamdict = {}
+spamdictlenwords = {}
 hamdict = {}
+hamdictlenwords = {}
 with open('sms-spam-corpus.csv', newline='') as f:
     reader = csv.DictReader(f, delimiter=',')
     for row in reader:
@@ -53,6 +69,14 @@ with open("hamdict.csv", "w") as outfile:
     writer = csv.DictWriter(outfile, fieldnames=hamdict)
     writer.writeheader()
     writer.writerow(hamdict)
+avgvaluespam=analyslenofword(spamdict, spamdictlenwords)
+avgvalueham=analyslenofword(hamdict, hamdictlenwords)
+plt.bar(spamdictlenwords.keys(), spamdictlenwords.values(), color='g')
+plt.title("Средняя длина слова: %.2f " % avgvaluespam)
+plt.show()
+plt.bar(hamdictlenwords.keys(), hamdictlenwords.values(), color='g')
+plt.title("Средняя длина слова: %.2f " % avgvalueham)
+plt.show()
 #with open("repaired.csv", "w") as outfile:
  #   for item in sorted(spamdict):
   #      csv.writer(outfile).writerows(spamdict[item])
@@ -61,9 +85,11 @@ with open("hamdict.csv", "w") as outfile:
 #print(spamdict)
 #analysis(spamlist,spamdict)
 #analysis(hamlist,hamdict)
-#for item in spamdict:
- #   print("'%s':%s" % (item, spamdict[item]))
-
+for item in sorted(spamdictlenwords):
+ print("'%s':%s" % (item, spamdictlenwords[item]))
+print("-------------------------------------------------------")
+for item in sorted(hamdictlenwords):
+    print("'%s':%s" % (item, hamdictlenwords[item]))
 #with open("repaired.csv", "w") as outfile:
     #csv.writer(outfile).writerows(slist)
 #np.savetxt('file_2', slist, delimiter=",")
